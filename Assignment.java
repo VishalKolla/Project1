dbed9ff6e0b889a86d8c0f31320b8d6a994d406c
vkolla <vkolla@192.168.1.10>
2017-10-21 12:09:40 -0400

    InitialCommit

---
+++ Assignment.java
@@ -0,0 +1,26 @@
+/*Chapter 1: Welcome to my day
+ * Programmer: V. Kolla
+ * Date: September 18 2017
+ * Filename: Welcome.java
+ * Purpose: This project displays my initials.
+ */
+public class Assignment
+{
+   	public static void main (String[]args)
+   	{
+          	System.out.println();
+          	System.out.println("\t\t\tvvvvvv               vvvvvv    kkkkk    kkkkk");
+          	System.out.println("\t\t\t  vvv                 vvv       kkk     kkk");
+          	System.out.println("\t\t\t   vvv               vvv        kkk    kkk");
+          	System.out.println("\t\t\t    vvv             vvv         kkk   kkk");
+          	System.out.println("\t\t\t     vvv           vvv          kkk  kkk");
+          	System.out.println("\t\t\t      vvv         vvv           kkk kkk");
+          	System.out.println("\t\t\t       vvv       vvv            kkkkkk");
+          	System.out.println("\t\t\t        vvv     vvv             kkk kkk");
+          	System.out.println("\t\t\t         vvv   vvv              kkk  kkk");
+          	System.out.println("\t\t\t          vvv vvv               kkk   kkk");
+          	System.out.println("\t\t\t           vvvvv                kkk    kkk");
+          	System.out.println("\t\t\t            vvv                 kkk     kkk");
+          	System.out.println("\t\t\t             v                 kkkkk    kkkkk");
+   	}
+}
